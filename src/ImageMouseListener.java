741bbca8605b7c7ccf544c9937f820c8b071b744
Daniel Davidovitch <underdrag1@gmail.com>
2024-06-28 19:46:53 +0300

    Changed Slider logic

--- src/ImageMouseListener.java
+++ src/ImageMouseListener.java
@@ -14,12 +14,19 @@ public class ImageMouseListener implements MouseMotionListener{
 
     @Override
     public void mouseDragged(MouseEvent e) {
-        this.moveSlider = e.getX() >= this.slider.getX() && e.getX() <= this.slider.getX() + this.slider.getWidth();
-
-        if (this.moveSlider) {
-            Point newPoint = new Point(e.getX(), (int) this.slider.getY());
-            this.slider.setLocation(newPoint);
+        if(e.getX() < e.getComponent().getX() + e.getComponent().getWidth()) {
+            this.moveSlider = e.getX() >= this.slider.getX() - 30 && e.getX() <= this.slider.getX() + this.slider.getWidth() + 30
+                            && e.getY() >= 0 && e.getY() <= e.getComponent().getHeight();
+            if(this.moveSlider) {
+                this.slider.setLocation(new Point(e.getX(), (int) this.slider.getY()));
+            }
         }
+//        this.moveSlider = e.getX() >= this.slider.getX() && e.getX() <= this.slider.getX() + this.slider.getWidth();
+//
+//        if (this.moveSlider) {
+//            Point newPoint = new Point(e.getX(), (int) this.slider.getY());
+//            this.slider.setLocation(newPoint);
+//        }
     }
 
     @Override
